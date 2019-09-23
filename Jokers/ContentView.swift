42dee00ecdddec8f6db02de7d77e3527306283b9
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-09-22 20:20:36 -0700

    Init + Joke

--- Jokers/ContentView.swift
+++ Jokers/ContentView.swift
@@ -8,9 +8,26 @@
 
 import SwiftUI
 
+struct Joke {
+    var setup: String
+    var punchline: String
+    var rating: String
+}
+
 struct ContentView: View {
+    
+    let jokes = [
+        Joke(setup: "What's a cow's favorite place", punchline: "A mooseum", rating: "Silence"),
+        Joke(setup: "What's brown and sticky?", punchline: "A Stick", rating: "Sigh"),
+        Joke(setup: "What's orange and sounds like a parrot", punchline: "A carrot", rating: "Smirk")
+    ]
+    
     var body: some View {
-        Text("Hello World")
+        List {
+            ForEach(jokes, id: \.setup) { joke in
+                Text(joke.setup)
+            }
+        }
     }
 }
 
