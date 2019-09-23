7875046b879a6da6be126d923430dd97f974e1a8
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-09-22 20:14:18 -0700

    Initial Commit

---
+++ Jokers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Jokers
+//
+//  Created by Prudhvi Gadiraju on 9/22/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
